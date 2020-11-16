bc6495ca90b9f7689b00b452e7a61182b3748d45
solaldunckel <solaldunckel@gmail.com>
2020-11-16 16:17:37 +0100

    Minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# BetterJokUI
+# SoulbindTalents
+
+SoulbindTalents will integrate the SoulbindUI directly inside the Talent Frame.
+
+## v0.1b
+* First beta version
+* Option to disable fx (Shake, Link animation ...)
+* Replaced Item Level by Rank on the Conduit List
--- SoulbindTalents.lua
+++ SoulbindTalents.lua
@@ -8,9 +8,14 @@ LibStub("AceHook-3.0"):Embed(SoulbindTalents)
 SOULBIND_TAB = 4
 
 function SoulbindTalents:OnEnable()
-	self:RegisterEvent("SOULBIND_FORGE_INTERACTION_STARTED")
 	self:RegisterEvent("SOULBIND_FORGE_INTERACTION_ENDED")
 	self:RegisterEvent("ADDON_LOADED")
+
+	self.disableFX = true
+
+	if self.disableFX then
+		SetCVar("ShakeStrengthUI", 0)
+	end
 end
 
 function SoulbindTalents:AppendSoulbindFrame()
@@ -27,6 +32,7 @@ function SoulbindTalents:AppendSoulbindFrame()
 	SoulbindViewer.ShadowLeft:Hide()
 	SoulbindViewer.ShadowBottom:Hide()
 	SoulbindViewer.ShadowRight:Hide()
+
 	SoulbindViewer.Background:ClearAllPoints()
 	SoulbindViewer.Background:SetParent(PlayerTalentFrameSoulbind)
 	SoulbindViewer.Background:SetDrawLayer("BACKGROUND", -1)
@@ -37,9 +43,16 @@ function SoulbindTalents:AppendSoulbindFrame()
 	SoulbindViewer.ConduitList.ScrollBar:SetPoint("TOPRIGHT", SoulbindViewer.ConduitList, -6, 0)
 	SoulbindViewer.ConduitList.ScrollBar:SetPoint("BOTTOMRIGHT", SoulbindViewer.ConduitList, -6, 0)
 
-	-- SoulbindViewer.Fx:Hide()
-	-- SoulbindViewer.ActivateFX:SetAlpha(0)
-	-- SoulbindViewer.ActivateFX2:SetAlpha(0)
+	if self.disableFX then
+		SoulbindViewer.Fx:Hide()
+		SoulbindViewer:SetSheenAnimationsPlaying(false)
+	end
+
+	SoulbindViewer.ActivateFX:ClearAllPoints()
+	SoulbindViewer.ActivateFX:SetPoint("TOPLEFT", SoulbindViewer, "TOPLEFT", 197, -22)
+
+	SoulbindViewer.ActivateFX2:ClearAllPoints()
+	SoulbindViewer.ActivateFX2:SetPoint("TOPLEFT", SoulbindViewer, "TOPLEFT", 197, -22)
 
 	SoulbindViewer.Background2:ClearAllPoints()
 	SoulbindViewer.Background2:SetParent(PlayerTalentFrameSoulbind)
@@ -72,6 +85,25 @@ function SoulbindTalents:RestoreSoulbindFrame()
 	SoulbindViewer.Background:SetParent(SoulbindViewer)
 	SoulbindViewer.Background:SetPoint("TOPLEFT", SoulbindViewer, "TOPLEFT", 26, -25)
 
+	SoulbindViewer.ConduitList.ScrollBar:ClearAllPoints()
+	SoulbindViewer.ConduitList.ScrollBar:SetPoint("TOPRIGHT", SoulbindViewer.ConduitList, "TOPRIGHT", -6, -36)
+
+	if self.disableFX then
+		SoulbindViewer.Fx:Hide()
+	end
+
+	SoulbindViewer.ActivateFX:ClearAllPoints()
+	SoulbindViewer.ActivateFX:SetPoint("TOPLEFT", SoulbindViewer, "TOPLEFT", 212, -45)
+
+	SoulbindViewer.ActivateFX2:ClearAllPoints()
+	SoulbindViewer.ActivateFX2:SetPoint("TOPLEFT", SoulbindViewer, "TOPLEFT", 212, -45)
+
+	SoulbindViewer.Background2:ClearAllPoints()
+	SoulbindViewer.Background2:SetParent(SoulbindViewer)
+
+	SoulbindViewer.BackgroundBlackOverlay:SetParent(SoulbindViewer)
+	SoulbindViewer.BackgroundBlackOverlay:SetDrawLayer("BACKGROUND", -1)
+
 	SoulbindViewer.ActivateSoulbindButton:ClearAllPoints()
 	SoulbindViewer.ActivateSoulbindButton:SetPoint("TOP", SoulbindViewer, "BOTTOM", -93, 72)
 
@@ -86,7 +118,7 @@ function SoulbindTalents:ShowSoulbindsTab()
 		local covenantData = C_Covenants.GetCovenantData(soulbindData.covenantID);
 		SoulbindViewer:Init(covenantData, soulbindData);
 
-		SoulbindTalents:AppendSoulbindFrame()
+		self:AppendSoulbindFrame()
 
 		SoulbindViewer:Show();
 	end
@@ -102,6 +134,12 @@ function SoulbindTalents:HideSoulbindsTab()
 		SoulbindViewer:Hide();
 		ItemButtonUtil.TriggerEvent(ItemButtonUtil.Event.ItemContextChanged);
 	end
+	PlayerTalentFrameTab1:Show()
+	PlayerTalentFrameTab2:Show()
+	if self.petTab then
+		PlayerTalentFrameTab3:Show()
+	end
+	PlayerTalentFrameTab4:Show()
 end
 
 function SoulbindTalents:PlayerTalentFrame_Refresh()
@@ -139,14 +177,16 @@ function SoulbindTalents:PlayerTalentFrame_OnHide()
 end
 
 function SoulbindTalents:SoulbindViewer_OnOpen()
-	if not PlayerTalentFrameSoulbind:IsShown() then
+	if not PlayerTalentFrame:IsShown() then
 		ToggleTalentFrame(4)
+	elseif PlayerTalentFrame:IsShown() then
+		local selectedTab = PanelTemplates_GetSelectedTab(PlayerTalentFrame);
+		if (selectedTab ~= SOULBIND_TAB) then
+			PanelTemplates_SetTab(PlayerTalentFrame, 4)
+			self:PlayerTalentFrame_Refresh()
+		end
 	end
-end
-
-function SoulbindTalents:SOULBIND_FORGE_INTERACTION_STARTED()
-	self.forge = true
-	if (self.forge) then
+	if C_Soulbinds.CanModifySoulbind() then
 		PlayerTalentFrameTab1:Hide()
 		PlayerTalentFrameTab2:Hide()
 		if self.petTab then
@@ -156,14 +196,57 @@ function SoulbindTalents:SOULBIND_FORGE_INTERACTION_STARTED()
 	end
 end
 
-function SoulbindTalents:SOULBIND_FORGE_INTERACTION_ENDED()
-	self.forge = false
-	PlayerTalentFrameTab1:Show()
-	PlayerTalentFrameTab2:Show()
-	if self.petTab then
-		PlayerTalentFrameTab3:Show()
+function SoulbindTalents:TooltipRank(conduit)
+	local rank = conduit:GetConduitRank()
+	if rank > 0 then
+		GameTooltip_AddNormalLine(GameTooltip, "Rank " .. conduit:GetConduitRank());
+		GameTooltip:Show()
 	end
-	PlayerTalentFrameTab4:Show()
+end
+
+function SoulbindTalents:ConduitRank(conduit, conduitData)
+	local itemID = conduitData.conduitItemID;
+	local item = Item:CreateFromItemID(itemID);
+	local itemCallback = function()
+		conduit.ConduitName:SetSize(150, 30);
+		conduit.ConduitName:SetText(item:GetItemName());
+		conduit.ConduitName:SetHeight(conduit.ConduitName:GetStringHeight());
+
+		local yOffset = conduit.ConduitName:GetNumLines() > 1 and -6 or 0;
+		conduit.ConduitName:ClearAllPoints();
+		conduit.ConduitName:SetPoint("BOTTOMLEFT", conduit.Icon, "RIGHT", 10, yOffset);
+		conduit.ConduitName:SetWidth(150);
+
+		conduit.ItemLevel:SetPoint("TOPLEFT", conduit.ConduitName, "BOTTOMLEFT", 0, 0);
+		conduit.ItemLevel:SetText("Rank " .. conduitData.conduitRank);
+	end;
+	item:ContinueOnItemLoad(itemCallback);
+end
+
+function SoulbindTalents:AnimationFX(viewer)
+	if self.disableFX then
+		viewer.ForgeSheen.Anim:SetPlaying(false);
+		viewer.BackgroundSheen1.Anim:SetPlaying(false);
+		viewer.BackgroundSheen2.Anim:SetPlaying(false);
+		viewer.GridSheen.Anim:SetPlaying(false);
+		viewer.BackgroundRuneLeft.Anim:SetPlaying(false);
+		viewer.BackgroundRuneRight.Anim:SetPlaying(false);
+		viewer.ConduitList.Fx.ChargeSheen.Anim:SetPlaying(false);
+	end
+end
+
+function SoulbindTalents:NodeFX(viewer)
+	if self.disableFX then
+		viewer.FlowAnim1:Stop();
+		viewer.FlowAnim2:Stop();
+		viewer.FlowAnim3:Stop();
+		viewer.FlowAnim4:Stop();
+		viewer.FlowAnim5:Stop();
+		viewer.FlowAnim6:Stop();
+	end
+end
+
+function SoulbindTalents:SOULBIND_FORGE_INTERACTION_ENDED()
 	self:HideSoulbindsTab()
 	if PlayerTalentFrame:IsShown() then
 		ToggleTalentFrame(4)
@@ -178,16 +261,13 @@ function SoulbindTalents:ADDON_LOADED(_, addon)
 			self:SecureHook('PlayerTalentFrame_Close')
 			self:HookScript(PlayerTalentFrame, "OnHide", "PlayerTalentFrame_OnHide")
 
-			local anchorTab = PlayerTalentFrameTab2
-
 			local _, playerClass = UnitClass("player");
 			if (playerClass == "HUNTER") then
 				self.petTab = true
-				anchorTab = PlayerTalentFrameTab3
 			end
 
 			local PlayerTalentFrameTab4 = CreateFrame("Button", "$parentTab4", PlayerTalentFrame, "PlayerTalentTabTemplate")
-			PlayerTalentFrameTab4:SetPoint("LEFT", anchorTab, "RIGHT", -15, 0)
+			PlayerTalentFrameTab4:SetPoint("LEFT", self.petTab and PlayerTalentFrameTab3 or PlayerTalentFrameTab2, "RIGHT", -15, 0)
 			PlayerTalentFrameTab4:SetText("Soulbinds")
 			PlayerTalentFrameTab4:SetID(SOULBIND_TAB)
 
@@ -205,6 +285,10 @@ function SoulbindTalents:ADDON_LOADED(_, addon)
 	elseif addon == "Blizzard_Soulbinds" then
 		if UIParentLoadAddOn("Blizzard_TalentUI") then
 			self:Hook(SoulbindViewer, "Open", "SoulbindViewer_OnOpen", true)
+			self:SecureHook(SoulbindConduitNodeMixin, "LoadTooltip", "TooltipRank")
+			self:SecureHook(ConduitListConduitButtonMixin, "Init", "ConduitRank")
+			self:SecureHook(SoulbindViewer, "SetSheenAnimationsPlaying", "AnimationFX")
+			self:SecureHook(SoulbindTreeNodeLinkMixin, "SetState", "NodeFX")
 		end
 	end
 end
